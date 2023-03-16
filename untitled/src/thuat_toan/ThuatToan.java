5142f465dbff1eee3b052e72ee615b3a7667b239
letrunghaiduong <letrunghaiduong260701@gmail.com>
2023-03-16 22:21:08 +0700

    update

--- untitled/src/thuat_toan/ThuatToan.java
+++ untitled/src/thuat_toan/ThuatToan.java
@@ -1,16 +1,26 @@
 package thuat_toan;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ThuatToan {
     public static void main(String[] args) {
         int[] arr1 = {3, 4, 5, 7, 9, 7, 4, 3, 6, 2, 7, 9};
         int[] arr2 = {3, 4, 1, 9, 4, 8, 6, 2, 7};
+
         Map<Integer, Integer> map = new HashMap<>();
+
         List<Integer> result = new ArrayList<>();
 
+        for (int item1 : arr1) {
+            map.put(item1, 1);
+        }
+
+        for (int item2 : arr2) {
+            if (map.containsKey(item2)) {
+                result.add(item2);
+                map.remove(item2);
+            }
+        }
+        System.out.println("Kết quả" + result);
     }
 }
