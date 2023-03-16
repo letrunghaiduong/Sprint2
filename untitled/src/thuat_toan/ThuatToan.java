42e1bee8f07e29c2bd2dd0ca417ffab6a24eca3b
letrunghaiduong <letrunghaiduong260701@gmail.com>
2023-03-16 20:47:44 +0700

    update

---
+++ untitled/src/thuat_toan/ThuatToan.java
@@ -0,0 +1,16 @@
+package thuat_toan;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ThuatToan {
+    public static void main(String[] args) {
+        int[] arr1 = {3, 4, 5, 7, 9, 7, 4, 3, 6, 2, 7, 9};
+        int[] arr2 = {3, 4, 1, 9, 4, 8, 6, 2, 7};
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+    }
+}
